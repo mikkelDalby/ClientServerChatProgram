7950dfb31dfbcbf135725211e56bba14483fe690
Mikkel Dalby Nielsen <mikkel@dalbynielsen.dk>
2018-10-06 13:44:57 +0200

    Client can now connect to server with the correct string and server responds with ok message

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -6,30 +6,68 @@ import java.util.*;
 
 public class ClientMain {
     private static InetAddress host;
-    private static final int PORT = 1234;
+    private static int port;
+
+    private static boolean isConnected = false;
+
+    //Set up stream for keyboard entry
+    private static Scanner userEntry = new Scanner(System.in);
 
     public static void main(String[] args) {
-        try {
-            host = InetAddress.getByName("127.0.0.1");
-            //host = InetAddress.getLocalHost();
-        } catch (UnknownHostException uhEx){
-            System.out.println("\nHost ID not found!\n");
-            System.exit(1);
-        }
-        sendMessages();
+        connectToServer();
     }
 
-    private static void sendMessages(){
+    private static void connectToServer(){
         Socket socket = null;
+        Scanner networkInput;
+        PrintWriter networkOutput;
+        do {
+                System.out.println("Connect to server: JOIN <<username>>, <<serverIp>>:<<serverPort>>");
+                String message = userEntry.nextLine();
+                String[] parameters = splitInputString(message);
 
+            try {
+                host = InetAddress.getByName(parameters[2]);
+                socket = new Socket(host, Integer.parseInt(parameters[3]));
+                networkInput = new Scanner(socket.getInputStream());
+                networkOutput = new PrintWriter(socket.getOutputStream(), true);
+
+                networkOutput.println(message);
+                String response = networkInput.nextLine();
+
+                if (response.equals("J_OK")){
+                    isConnected = true;
+                } else {
+                    System.out.println("Server did'nt respond try again");
+                }
+
+            } catch (UnknownHostException uhEx) {
+                System.out.println("\nHost not found!\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Message not valid");
+            }
+        } while (!isConnected);
+            sendMessages(socket);
+    }
+
+    private static String[] splitInputString(String input){
         try {
-            socket = new Socket(host, PORT);
+            String[] parameters = input.split(" |:");
+            parameters[1] = parameters[1].replace(",", "");
+            return parameters;
+        } catch (ArrayIndexOutOfBoundsException e){
+            String[] array = new String[1];
+            array[0] = "notSet";
+            return array;
+        }
+    }
 
+    private static void sendMessages(Socket socket){
+        try {
             Scanner networkInput = new Scanner(socket.getInputStream());
             PrintWriter networkOutput = new PrintWriter(socket.getOutputStream(), true);
-
-            //Set up stream for keyboard entry
-            Scanner userEntry = new Scanner(System.in);
 
             String message, response;
 
